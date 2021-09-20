2300ed571a0ba2f30c097a0a0df627c50e61792f
CORP\s.prokudaylo <s.prokudaylo@npcmr.ru>
2021-09-20 17:01:00 +0300

    Company icon added

--- PGC/PGC/PGC.cpp
+++ PGC/PGC/PGC.cpp
@@ -1,7 +1,18 @@
+#include <QIcon>
+
+
 #include "PGC.h"
 
 PGC::PGC(QWidget *parent)
     : QWidget(parent)
 {
+    QIcon icon;
 
+    QIcon::Mode mode = QIcon::Normal;
+    QIcon::State state = QIcon::On;
+
+    const QString fileName = "./centaur-logo.png";
+    QImage image(fileName);
+    if (!image.isNull())
+        icon.addPixmap(QPixmap::fromImage(image), mode, state);
 }
--- PGC/PGC/main.cpp
+++ PGC/PGC/main.cpp
@@ -1,10 +1,26 @@
 #include "PGC.h"
+
 #include <QtWidgets/QApplication>
+#include <QIcon>
+#include <QDebug>
+
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+using namespace filesystem;
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString workingDirectory = QString::fromWCharArray( weakly_canonical(path(argv[0])).parent_path().c_str() );
+    a.setWindowIcon(QIcon(workingDirectory + "\\centaur-icon.png"));
+
     PGC w;
     w.show();
+
     return a.exec();
 }
