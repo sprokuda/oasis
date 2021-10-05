718e1250ca6eb1a68f544e0f0a51924561eb9c7a
CORP\s.prokudaylo <s.prokudaylo@npcmr.ru>
2021-10-05 10:41:05 +0300

    Password is temoporary deactivated

--- PGC/PGC/main.cpp
+++ PGC/PGC/main.cpp
@@ -23,16 +23,16 @@ int main(int argc, char *argv[])
     app.setWindowIcon(QIcon(workingDirectory + "\\centaur-icon.png"));
 
     PGC w;
-
+    w.show();
     QFont font("Calibri", 10);
     int buttonHeight = 24;
 
-    QtPassword qp(font, buttonHeight);
+    //QtPassword qp(font, buttonHeight);
 
-    QObject::connect(&qp, SIGNAL(passwordAccepted()), &w, SLOT(show()));
-    QObject::connect(&qp, SIGNAL(passwordRejected()), &app, SLOT(quit()));
+    //QObject::connect(&qp, SIGNAL(passwordAccepted()), &w, SLOT(show()));
+    //QObject::connect(&qp, SIGNAL(passwordRejected()), &app, SLOT(quit()));
 
-    qp.show();
+    //qp.show();
 
     return app.exec();
 }
