f718561b06d06d6de58bdb79c17a7dc78a6a3a94
sprokuda <sprokuda@gmail.com>
2021-10-20 12:21:39 +0300

    modification of cout forwarding

--- PGC/PGC/main.cpp
+++ PGC/PGC/main.cpp
@@ -17,10 +17,33 @@ QString workingDirectory;
 
 QFont workingFont;
 
+void MessageOutput(QtMsgType type, const QMessageLogContext& context, const QString& msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg:
+        cout << "QtDebugMsg: " << msg.toStdString() << endl;
+        break;
+    case QtInfoMsg:
+        cout << "QtInfoMsg: " << msg.toStdString() << endl;
+        break;
+    case QtWarningMsg:
+        cout << "QtWarningMsg: " << msg.toStdString() << endl;
+        break;
+    case QtCriticalMsg:
+        cout << "QtCriticalMsg: " << msg.toStdString() << endl;
+        break;
+    case QtFatalMsg:
+        cout << "QtFatalMsg: " << msg.toStdString() << endl;
+//        fprintf(stdout, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        abort();
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(MessageOutput);
     QApplication app(argc, argv);
-    ThreadLogStream qout(std::cout);
 
     workingFont = QFont("Calibri", 10);
 
@@ -29,6 +52,7 @@ int main(int argc, char *argv[])
     app.setWindowIcon(QIcon(workingDirectory + "\\centaur-icon.png"));
 
     PGC w;
+    ThreadLogStream qout(cout);
     QObject::connect(&qout, SIGNAL(sendLogString(QString)), &w, SLOT(updateLog(QString)));
     w.show();
     w.initialLoad();
