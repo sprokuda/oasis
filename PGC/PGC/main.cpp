cb98874613acab557e4c4427f3b32d7372351d69
DESKTOP-FHST0MP\sprok <s.prokudaylo@npcmr.ru>
2021-11-05 11:11:57 +0300

    update of main

--- PGC/PGC/main.cpp
+++ PGC/PGC/main.cpp
@@ -43,50 +43,6 @@ void MessageOutput(QtMsgType type, const QMessageLogContext& context, const QStr
 }
 
 
-void CreateLogFile()
-{
-    const char* dir1 = "OASIS";
-    const char* dir2 = "PGC";
-
-    QDir dir;
-    dir.cd("C:/");
-
-    if (!QDir(dir1).exists())
-        cout << "QDir returned: " << dir.mkdir(dir1) << endl;
-    dir.cd(dir1);
-
-    const QFileInfo outputDir1(dir.absolutePath().toStdString().c_str());
-    if ((!outputDir1.exists()) || (!outputDir1.isDir()) || (!outputDir1.isWritable()))
-    {
-        cout << "output directory does not exist, is not a directory, or is not writeable"
-            << "\t" << outputDir1.absoluteFilePath().toStdString() << endl;
-    }
-
-    if (!QDir(dir2).exists())
-        cout << "QDir returned: " << dir.mkdir(dir2) << endl;
-    dir.cd(dir2);
-
-    const QFileInfo outputDir2(dir.absolutePath().toStdString().c_str());
-    if ((!outputDir2.exists()) || (!outputDir2.isDir()) || (!outputDir2.isWritable()))
-    {
-        cout << "output directory does not exist, is not a directory, or is not writeable"
-            << "\t" << outputDir2.absoluteFilePath().toStdString() << endl;
-    }
-
-    QString fileName = QDate::currentDate().toString("yyyyMMdd") + "__" + QTime::currentTime().toString("hh-mm-ss-zzz") + QString(".log");
-    cout << (dir.absolutePath() + "\\" + fileName).toStdString() << endl;
-    fflush(stdout);
-
-    QFile file = QFile(dir.absolutePath() + "\\" + fileName);
-
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
-    {
-        file.close();
-    }
-}
-
-
-
 
 
 int main(int argc, char *argv[])
@@ -94,52 +50,29 @@ int main(int argc, char *argv[])
     ThreadLogStream qout(cout);
     qInstallMessageHandler(MessageOutput);
     QApplication app(argc, argv);
-
-    logOfPGC log;
-    log.openLog("");
-tryOpenLog:
-    if(!log.isOpened())
-    {
-        QMessageBox msgBox(nullptr);
-        msgBox.setText("Log file can not be created");
-        msgBox.setIcon(QMessageBox::Warning);
-        auto* open = msgBox.addButton("Try to open again", QMessageBox::ActionRole);
-        auto* exit = msgBox.addButton("Exit Program", QMessageBox::ActionRole);
-
-        msgBox.exec();
-
-        if (msgBox.clickedButton() == open)
-        {
-            log.openLog(""); goto tryOpenLog;
-        }
-        else if (msgBox.clickedButton() == exit)
-        {
-            return 0;
-        }
-    }
-
-    QObject::connect(&qout, SIGNAL(sendLogString(QString)), &log, SLOT(appendString(QString)));
-
-    
+    app.setWindowIcon(QIcon(":PGC/resources/centaur-icon.png"));
 
     workingFont = QFont("Calibri", 10);
 
+    QtPassword qp(workingFont);
+    qp.show();
+
+    logOfPGC log_of_pgc;
+    PGC w;
+
+    QObject::connect(&qp, SIGNAL(passwordAccepted()), &log_of_pgc, SLOT(openLog()));
+    QObject::connect(&qp, SIGNAL(passwordRejected()), &app, SLOT(quit()));
+    QObject::connect(&log_of_pgc, SIGNAL(noLogFileOpened()), &app, SLOT(quit()));
+    QObject::connect(&log_of_pgc, &logOfPGC::logFileOpened, [&qout, &log_of_pgc, &w]() 
+        {
+            QObject::connect(&qout, SIGNAL(sendLogString(QString)), &log_of_pgc, SLOT(appendString(QString)));
+            w.show();
+            QObject::connect(&qout, SIGNAL(sendLogString(QString)), &w, SLOT(updateLog(QString)));
+            w.initialLoad();
+        });
+
     workingDirectory = QString::fromWCharArray( weakly_canonical(path(argv[0])).parent_path().c_str() );
     qDebug() << workingDirectory;
-    app.setWindowIcon(QIcon(":PGC/resources/centaur-icon.png"));
-
-    PGC w;
-    QObject::connect(&qout, SIGNAL(sendLogString(QString)), &w, SLOT(updateLog(QString)));
-    w.show();
-    w.initialLoad();
-    //QFont font("Calibri", 10);
-    //int buttonHeight = 24;
-    //QtPassword qp(font, buttonHeight);
-
-    //QObject::connect(&qp, SIGNAL(passwordAccepted()), &w, SLOT(show()));
-    //QObject::connect(&qp, SIGNAL(passwordRejected()), &app, SLOT(quit()));
-
-    //qp.show();
 
     return app.exec();
 }
